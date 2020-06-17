b9fdda3fa3f0d2fb14f5c9b2dfb488024dee98e5
www.centos.cn <lucky@centoscn.vip>
2020-06-17 15:43:58 +0800

    判断用户和系统
    Signed-off-by: www.centos.cn <lucky@centoscn.vip>

--- docker.sh
+++ docker.sh
@@ -3,6 +3,42 @@
 # Copyright (c) 2020
 # Gmail: lucky@centoscn.vip
 # blog:  www.centoscn.vip
+flag=0
+echo -ne "root   Check \t........................ "
+isRoot=`id -u -n | grep root | wc -l`
+if [ "x$isRoot" == "x1" ];then
+  echo -e "[\033[32m OK \033[0m]"
+else
+  echo -e "[\033[31m ERROR \033[0m] 请用 root 用户执行安装脚本"
+  flag=1
+fi
+
+#操作系统检测
+echo -ne "CentOS7 Check \t........................ "
+if [ -f /etc/redhat-release ];then
+  osVersion=`cat /etc/redhat-release | grep -oE '[0-9]+\.[0-9]+'`
+  majorVersion=`echo $osVersion | awk -F. '{print $1}'`
+  if [ "x$majorVersion" == "x" ];then
+    echo -e "[\033[31m ERROR \033[0m] 操作系统类型版本不符合要求，请使用 CentOS 7 64 位版本"
+    flag=1
+  else
+    if [[ $majorVersion == 7 ]];then
+      is64bitArch=`uname -m`
+      if [ "x$is64bitArch" == "xx86_64" ];then
+         echo -e "[\033[32m OK \033[0m]"
+      else
+         echo -e "[\033[31m ERROR \033[0m] 操作系统必须是 64 位的，32 位的不支持"
+         flag=1
+      fi
+    else
+      echo -e "[\033[31m ERROR \033[0m] 操作系统类型版本不符合要求，请使用 CentOS 7"
+      flag=1
+    fi
+  fi
+else
+    echo -e "[\033[31m ERROR \033[0m] 操作系统类型版本不符合要求，请使用 CentOS 7"
+    flag=1
+fi
 echo "安装基本依赖"
 {
  yum install -y yum-utils device-mapper-persistent-data lvm2
